229cc51b25a8c7acb97754a6752922e97614a528
Krishna Vamsi <nvgkvamsi@gmail.com>
2022-08-25 16:37:43 -0700

    updating ./terraform/immutable-app-ami

--- sg.tf
+++ sg.tf
@@ -8,7 +8,7 @@ resource "aws_security_group" "main" {
     from_port   = 22
     to_port     = 22
     protocol    = "TCP"
-    cidr_blocks = [0.0.0.0/0]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
